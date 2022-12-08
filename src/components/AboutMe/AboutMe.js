22692c2500e7a4296350e0e87e7591a76f9d4dc7
Sami Samara <samisamara99@yahoo.com>
2022-12-08 02:34:23 -0600

    added download resume functionality

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -2,8 +2,9 @@ import React from "react";
 import "./AboutMe.css";
 import SkillBar from "../SkillBar/SkillBar.js";
 import selfPicture from "../../images/selfPicture.jpeg";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import resume from "../../images/Samara_Sami_Resume.pdf"
 
 const AboutMe = () => {
   return (
@@ -13,11 +14,11 @@ const AboutMe = () => {
         <div className="introSection">
           <img id="selfPicture" className="rounded-circle" src={selfPicture} />
           <p className="introText">
-            Hello! I'm a software engineer seeking employment in the computer science
-            industry. I graduated in Saint Xavier University with my Master's
-            degree in Computer Science. My primary goal is software engineering,
-            however, I also have a great interest in UX design, mobile
-            applications, cyber security, and many others.
+            Hello! I'm a software engineer seeking employment in the computer
+            science industry. I graduated in Saint Xavier University with my
+            Master's degree in Computer Science. My primary goal is software
+            engineering, however, I also have a great interest in UX design,
+            mobile applications, cyber security, and many others.
           </p>
         </div>
         <Row>
@@ -25,11 +26,14 @@ const AboutMe = () => {
             <div className="profileSection">
               <h3 className="segmentTitle">PROFILE</h3>
               <p className="aboutText">
-                I am a new graduate, and currently looking for full time positions
-                in Computer Science and Software Egineering. If you are interested
-                in me, please reach out via <a className="anchor" href="mailto:samisamara548@gmail.com">samisamara548@gmail.com</a>
+                I am a new graduate, and currently looking for full time
+                positions in Computer Science and Software Egineering. If you
+                are interested in me, please reach out via{" "}
+                <a className="anchor" href="mailto:samisamara548@gmail.com">
+                  samisamara548@gmail.com
+                </a>
               </p>
-              
+
               <div className="personalDetail">
                 <h4 className="detailLabel">FULL NAME</h4>
                 <p className="detail">Sami Samara</p>
@@ -48,9 +52,9 @@ const AboutMe = () => {
             <div className="skills">
               <h3 className="segmentTitle">SKILLS</h3>
               <p>
-                I have developed my skills in Computer Science over the years, as
-                well as other varying skills centered around design. Below are
-                highlights of those technical skills.
+                I have developed my skills in Computer Science over the years,
+                as well as other varying skills centered around design. Below
+                are highlights of those technical skills.
               </p>
               <SkillBar />
             </div>
@@ -58,7 +62,9 @@ const AboutMe = () => {
         </Row>
         <div className="aboutMeButtons">
           <button className="button btn-1">Hire Me</button>
-          <button className="button">Download Resume</button>
+          <a href={resume} download className="button">
+            Download Resume
+          </a>
         </div>
       </div>
     </div>
