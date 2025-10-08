0598d84be8ae63e6f262c7cc5cab0e781f81395c
Sami Samara <samisamara99@yahoo.com>
2025-10-08 01:09:45 -0500

    updated work timeline items

--- src/components/WorkTimeline.js
+++ src/components/WorkTimeline.js
@@ -4,26 +4,19 @@ import WorkElement from "./WorkElement";
 
 const jobs = [
   {
-    jobTitle: 'Intern Backend Engineer',
-    business: 'Changed App',
-    date: 'October 2023 - Present',
-    description: "Designed a comprehensive business admin application for admins to view end-user stats, enhancing business analytics, engineered backend functions and a Firebase database segregating user data by ABA numbers, optimizing client data management Spearheaded the creation of an intuitive and user-friendly interface for the Business Admin App, significantly enhancing user experience and engagement with the platform Implemented a secure sign-in process, enhancing user trust and app security",
+    jobTitle: 'Software Engineer',
+    business: 'Urban Communications',
+    date: 'April 2025 - Present',
+    description: "Built a LaTeX–Python pipeline to auto-generate PDF template documents from HTML form data, Integrated SMS with Opt-In/Opt-Out functionality into a CRM using PHP, Created CRM workflows to send SMS/email templates, auto-assign cases, and delete obsolete data, Migrated business data from a legacy CRM to a new system while preserving integrity, Optimized backend services to support seamless data flow, reducing server load times from 500ms to 120ms",
     key: "w1"
   },
   {
-    jobTitle: 'Contract Software Engineer',
-    business: 'Atlas IMS',
-    date: 'August 2021 - September 2022',
-    description: "Designed, developed, and maintained software applications to meet project requirements, collaborated with cross-functional teams to gather and analyze user needs and system specifications, worked with version control systems to manage and track changes in the codebase participated in code reviews, providing constructive feedback and ensuring code quality and adherence to coding standards",
+    jobTitle: 'Intern Backend Engineer',
+    business: 'Changed App',
+    date: 'October 2023 - October 2024',
+    description: "Designed a comprehensive business admin application for admins to view end-user stats, enhancing business analytics, engineered backend functions and a Firebase database segregating user data by ABA numbers, optimizing client data management Spearheaded the creation of an intuitive and user-friendly interface for the Business Admin App, significantly enhancing user experience and engagement with the platform Implemented a secure sign-in process, enhancing user trust and app security",
     key: "w2"
   },
-  {
-    jobTitle: 'Store Manager',
-    business: 'UPS',
-    date: 'March 2020 - Present',
-    description: "Managed UPS packaging and shipment operations. I also maintained privacy policies to abide with UPS's goal of maintaining customer's privacy, Operated all software programs on Point of Sale, MS Office System, and internal project management systems to meet with time sensitive tasks, and managed employment within the facility.",
-    key: "w3"
-  }
 ];
 
 const WorkTimeline = () => {
